173d62106b0cdb755aef552ccd616c5e43496ba5
Elvira Reshetnikova <83705191+ElviraResh@users.noreply.github.com>
2022-08-30 17:43:31 +0500

    Lesson1

--- src/components/Main/Сongratulation/Image.jsx
+++ src/components/Main/Сongratulation/Image.jsx
@@ -1,7 +1,7 @@
 import style from "./Сongratulation.module.css";
 
 const Image = () => (
-  <img className={style.img} src="" alt="" />
+  <img className={style.img} src="../../../img/card-bg.jpg" alt="" />
 );
 
 export default Image;
