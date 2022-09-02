a6b87bd5a747cdf6c426a19da6ac96995faee3cc
Elvira Reshetnikova <83705191+ElviraResh@users.noreply.github.com>
2022-09-02 17:53:33 +0500

    Lesson4

--- src/components/Main/Сongratulation/ImageCard.jsx
+++ src/components/Main/Сongratulation/ImageCard.jsx
@@ -3,10 +3,10 @@ import CardBG from "../../../img/card-bg.jpg";
 
 
 const ImageCard = () => {
-  const {image} = useSelector(state => state.image);
+  const {urlImg} = useSelector(state => state.image);
   //console.log(urlImg);
   return (
-    <img src={ CardBG || image.urlImg} alt="Фон открытки" width={840} height={520} />
+    <img src={ urlImg || CardBG } alt="Фон открытки" width={840} height={520} />
   )
 };
 
